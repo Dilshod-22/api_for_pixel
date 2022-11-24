8461c8b0d15d0ad9fcb936648926ca7c3e38acd0
Dilshod-22 <myprofileforgithub@gmail.com>
2022-11-24 14:07:54 +0500

    yangilandi

--- routers/auth.js
+++ routers/auth.js
@@ -2,6 +2,10 @@ const router = require("express").Router()
 const People = require("../models/user")
 const {inspection,inspectionLogin} = require("./function/auth")
 
+router.get("/allUser",async(req,res)=>{
+    const users = await People.FindOne()
+    res.json(users).status(200).end
+})
 
 router.post("/login",inspectionLogin,async(req,res)=>{
     const user = await People.findOne({email:req.body.email})
